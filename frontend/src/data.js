8874117e8555c43a73c051f313fbd74b0e87eb7b
kubracoskun <89079792+kubracoskun@users.noreply.github.com>
2021-09-16 23:34:17 +0300

    Create React App

---
+++ frontend/src/data.js
@@ -0,0 +1,76 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            _name:'Mutlu Olma Sanatı',
+            category:'book',
+            image:'/images/Product1.jpg',
+            price:10,
+            brand:'Can Yayınları',
+            rating:5,
+            numReviews:10,
+            description:'Can Yayınları - Mutlu Olma Sanatı - Arthur Schopenhauer'
+        },
+        {
+            _id:'2',
+            _name:'Güneşi Uyandıralım',
+            category:'book',
+            image:'/images/Product2.jpg',
+            price:27,
+            brand:'Can Yayınları',
+            rating:5,
+            numReviews:10,
+            description:'Can Yayınları - Güneşi Uyandıralım - Jose Mauro De Vasconcelos'
+        },
+        {
+            _id:'3',
+            _name:'Kurtlarla Koşan Kadınlar',
+            category:'book',
+            image:'/images/Product3.jpg',
+            price:10,
+            brand:'Ayrıntı Yayınları',
+            rating:5,
+            numReviews:10,
+            description:'Ayrıntı Yayınları - Kurtlarla Koşan Kadınlar - Clarissa Pinkola Estes'
+        },
+        {
+            _id:'4',
+            _name:'Mülksüzler',
+            category:'book',
+            image:'/images/Product4.jpg',
+            price:25,
+            brand:'Metis',
+            rating:5,
+            numReviews:10,
+            description:'Metis - Mülksüzler - Ursula K. Le Guin'
+        },
+        {
+            _id:'5',
+            _name:'Suç ve Ceza',
+            category:'book',
+            image:'/images/Product5.jpg',
+            price:22,
+            brand:'İş Bankası Yayınları',
+            rating:5,
+            numReviews:10,
+            description:'İş Bankası Yayınları - Suç ve Ceza - Fyodor Mihayloviç Dostoyevski'
+        },
+        {
+            _id:'6',
+            _name:'Siyah Kan',
+            category:'book',
+            image:'/images/Product6.jpg',
+            price:55,
+            brand:'Doğan Kitap',
+            rating:5,
+            numReviews:10,
+            description:'Doğan Kitap - Siyah Kan - Jean Christophe Grange '
+        },
+            
+            
+      
+
+    ]
+}
+
+export default data;
