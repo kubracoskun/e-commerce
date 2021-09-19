24857ddc781689ad32d7964c11ab34502a79f78d
kubracoskun <89079792+kubracoskun@users.noreply.github.com>
2021-09-19 15:00:46 +0300

    Build Product Screen
    Create HomeScreen and ProductScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Product from './components/Product';
-import data from'./data';
+import {BrowserRouter, Route} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
-    <div className="grid-container">
+      <BrowserRouter>
+      <div className="grid-container">
         <header className="row"> 
             <div>
                 <a className="brand" href="/">Amazon</a>
@@ -15,19 +17,14 @@ function App() {
             </div>
         </header>
         <main>
-            <div className="row center">
-                {
-                    data.products.map((product) => (
-                    <Product key={product._id} product={product}></Product>
-                    ))
-                }
-                
-                
-                
-            </div>  
+            <Route path="/product/:id" component={ProductScreen}></Route>
+            <Route path="/" component={HomeScreen} exact></Route>
+            
         </main>
         <footer className="row center">All right reserved</footer>
     </div>
+      </BrowserRouter>
+       
   );
 }
 
